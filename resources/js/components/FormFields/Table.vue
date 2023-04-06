89e27d39ff5d8958b886cf65d2a823b898bc39eb
vladislavbrum <109315773+vladislavbrum@users.noreply.github.com>
2023-04-06 15:12:43 +0300

    Create Table.vue

---
+++ resources/js/components/FormFields/Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative bg-30 bg-clip border border-60" :class="{ 'mr-12': editMode && canDelete }">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    canDelete: {
+      type: Boolean,
+      default: true,
+    },
+    editMode: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
