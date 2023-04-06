924dd1315e24bac80bb75a225d5ef6759a69adb4
vladislavbrum <109315773+vladislavbrum@users.noreply.github.com>
2023-04-06 15:12:22 +0300

    Create DetailField.vue

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,31 @@
+<template>
+  <PanelItem :index="index" :field="field">
+    <template #value>
+      <Table :edit-mode="false" class="overflow-hidden" v-if="theData.length > 0">
+        <div class="bg-white overflow-hidden">
+          <TableRow :disabled="true" :key="index" :row="row" v-for="(row, index) in theData" />
+        </div>
+      </Table>
+    </template>
+  </PanelItem>
+</template>
+
+<script>
+import TableRow from './FormFields/TableRow';
+import Table from './FormFields/Table';
+
+export default {
+  props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+  components: { Table, TableRow },
+
+  data: () => ({ theData: [] }),
+
+  created() {
+    let valuesArray = Array.isArray(this.field.value) ? this.field.value : JSON.parse(this.field.value);
+    if (!Array.isArray(valuesArray) || !valuesArray.length) valuesArray = [];
+
+    this.theData = _.map(valuesArray, cells => ({ cells }));
+  },
+};
+</script>
