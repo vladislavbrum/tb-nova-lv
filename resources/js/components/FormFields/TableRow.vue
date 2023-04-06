05f806e2de46b5dfde5d5283bbd341064f7b04f2
vladislavbrum <109315773+vladislavbrum@users.noreply.github.com>
2023-04-06 15:13:18 +0300

    Create TableRow.vue

---
+++ resources/js/components/FormFields/TableRow.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="flex items-center key-value-item">
+    <div class="flex flex-grow border-b border-50 key-value-fields">
+      <div
+        :key="`column-${index}`"
+        @click="handleColumnFieldFocus(index)"
+        class="flex-grow border-l border-50"
+        v-for="(cell, index) in row.cells"
+      >
+        <textarea
+          :class="{
+            'bg-white': !isEditable,
+            'hover:bg-20 focus:bg-white': isEditable,
+          }"
+          :disabled="!isEditable"
+          :dusk="`key-value-value-${index}`"
+          :key="cell.id"
+          @focus="handleColumnFieldFocus(index)"
+          class="font-mono text-sm block min-h-input w-full form-control form-input form-input-row py-4 text-90 min-h-full"
+          ref="columnFields"
+          v-model="row.cells[index]"
+        />
+      </div>
+    </div>
+
+    <div class="flex justify-center h-11 w-11 absolute" style="right: -40px" v-if="isEditable && canDelete">
+      <button
+        @click="$emit('remove-row', row.id)"
+        class="flex appearance-none cursor-pointer text-70 hover:text-danger active:outline-none active:shadow-outline focus:outline-none focus:shadow-outline"
+        style="align-items: center"
+        tabindex="-1"
+        title="Delete"
+        type="button"
+      >
+        <icon type="trash" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import autosize from 'autosize';
+
+export default {
+  props: {
+    index: Number,
+    row: Object,
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    canDelete: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  mounted() {
+    autosize(this.$refs.columnFields);
+  },
+
+  methods: {
+    handleColumnFieldFocus(index) {
+      this.$refs.columnFields[index].select();
+    },
+  },
+
+  computed: {
+    isEditable() {
+      return !this.readOnly && !this.disabled;
+    },
+  },
+};
+</script>
